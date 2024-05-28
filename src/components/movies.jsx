916ebdd293e702dab947c197a86533621f57242b
abhi <abhijeetkc01@gmail.com>
2024-05-28 09:36:15 +0545

    delete and CSS added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService'
+import './formovies.css';   // importing css file
 
 class Movies extends Component {
    
@@ -16,8 +17,39 @@ class Movies extends Component {
         return(
             <React.Fragment>
                 <h1>Movies List</h1>
-                
-        
+                <div className = "table-container">
+                    <table border="0">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Genre</th>
+                                <th>Stock</th>
+                                <th>Rate</th>
+                                <th>Removal Option</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {
+                                this.state.movies.map(movie => (
+                                    <tr key = {movie._id} class = 'table-row'>
+                                        <td>{movie.title}</td>
+                                        <td>{movie.genre.name}</td>
+                                        <td>{movie.numberInStock}</td>
+                                        <td>{movie.dailyRentalRate}</td>
+                                        <td>
+                                            <button
+                                                onClick = {() => this.handleDelete(movie)}
+                                                className = "btn btn-danger"
+                                            >Delete 
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </React.Fragment>
         )
     }
