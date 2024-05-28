8f213dfbd57e78b57e0ef7007290f8b2844fc629
abhi <abhijeetkc01@gmail.com>
2024-05-28 10:43:50 +0545

    done all parts

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -22,41 +22,45 @@ class Movies extends Component {
                     this.state.movies.length === 0 ?
                     (<p>There are no movies in the database.</p>)
                     :
-                    (<p>There are {this.state.movies.length} movies in the database.</p>)
-                }
-                <div className = "table-container">
-                    <table border="0">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th>Removal Option</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {
-                                this.state.movies.map(movie => (
-                                    <tr key = {movie._id} class = 'table-row'>
-                                        <td>{movie.title}</td>
-                                        <td>{movie.genre.name}</td>
-                                        <td>{movie.numberInStock}</td>
-                                        <td>{movie.dailyRentalRate}</td>
-                                        <td>
-                                            <button
-                                                onClick = {() => this.handleDelete(movie)}
-                                                className = "btn btn-danger"
-                                            >Delete 
-                                            </button>
-                                        </td>
+                    (
+                    <React.Fragment>
+                        
+                        <p>There are {this.state.movies.length} movies in the database.</p>
+                    
+                        <div className = "table-container">
+                            <table border="0">
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Genre</th>
+                                        <th>Stock</th>
+                                        <th>Rate</th>
+                                        <th>Removal Option</th>
                                     </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                                </thead>
+
+                                <tbody>
+                                    {
+                                        this.state.movies.map(movie => (
+                                            <tr key = {movie._id} class = 'table-row'>
+                                                <td>{movie.title}</td>
+                                                <td>{movie.genre.name}</td>
+                                                <td>{movie.numberInStock}</td>
+                                                <td>{movie.dailyRentalRate}</td>
+                                                <td>
+                                                    <button
+                                                        onClick = {() => this.handleDelete(movie)}
+                                                        className = "btn btn-danger"
+                                                    >Delete 
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                </React.Fragment>)}
             </React.Fragment>
         )
     }
