b27b085f9a73d3d28cb8fa330e62f0325c7c8bb9
abhi <abhijeetkc01@gmail.com>
2024-05-28 10:39:37 +0545

    display movies number fixed

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -14,10 +14,16 @@ class Movies extends Component {
     };
 
     render(){
+
         return(
             <React.Fragment>
                 <h1>Movies List</h1>
-                <p>There are {this.state.movies.length} movies in the database.</p>
+                {
+                    this.state.movies.length === 0 ?
+                    (<p>There are no movies in the database.</p>)
+                    :
+                    (<p>There are {this.state.movies.length} movies in the database.</p>)
+                }
                 <div className = "table-container">
                     <table border="0">
                         <thead>
